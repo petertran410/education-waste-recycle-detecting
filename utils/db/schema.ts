a0c459550cdd5c08aefd1a42cb9aed03efa77d4c
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-09-17 22:05:10 +0700

    Create database

---
+++ utils/db/schema.ts
@@ -0,0 +1,58 @@
+import {
+  integer,
+  varchar,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  jsonb,
+  boolean,
+} from "drizzle-orm/pg-core";
+
+export const Users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  name: varchar("name", { length: 255 }).notNull(),
+  timeCreate: timestamp("time_create").defaultNow().notNull(),
+});
+
+export const Reports = pgTable("reports", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id")
+    .references(() => Users.id)
+    .notNull(),
+  location: text("location").notNull(),
+  wasteType: varchar("waste_type", { length: 255 }).notNull(),
+  amount: varchar("amount", { length: 255 }).notNull(),
+  imageUrl: text("image_url"),
+  verificationResult: jsonb("verification_result"),
+  status: varchar("status", { length: 255 }).notNull().default("pending"),
+  timeCreate: timestamp("time_create").defaultNow().notNull(),
+  collectorId: integer("collector_id").references(() => Users.id),
+});
+
+export const Rewards = pgTable("rewards", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id")
+    .references(() => Users.id)
+    .notNull(),
+  points: integer("points").notNull().default(0),
+  timeCreate: timestamp("time_create").defaultNow().notNull(),
+  timeUpdate: timestamp("time_update").defaultNow().notNull(),
+  isAvailable: boolean("is_available").notNull().default(true),
+  description: text("description"),
+  name: varchar("name", { length: 255 }).notNull(),
+  collectionInfo: text("collection_info").notNull(),
+});
+
+export const CollectedWaste = pgTable("collected_wasted", {
+  id: serial("id").primaryKey(),
+  reportId: integer("report_id")
+    .references(() => Reports.id)
+    .notNull(),
+  collectedId: integer("collected_id")
+    .references(() => Users.id)
+    .notNull(),
+  collectionDate: timestamp("collection_date").notNull(),
+  status: varchar("status", { length: 255 }).default("collected "),
+});
