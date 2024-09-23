10ccaee1473ba788eb95192e6b3c129fc7ff55bc
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-09-24 00:44:38 +0700

    Done Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -35,6 +35,8 @@ import {
   markNotificationAsRead,
 } from "@/utils/db/actions";
 
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+
 const clientId = process.env.WEB3AUTH_API_KEY;
 
 const chainConfig = {
@@ -71,6 +73,7 @@ export default function Header({ onMenuClick, totalEarnings }: HeaderProps) {
   const pathName = usePathname();
   const [notification, setNotification] = useState<Notification[]>([]);
   const [balance, setBalance] = useState(0);
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   useEffect(() => {
     const init = async () => {
@@ -218,6 +221,7 @@ export default function Header({ onMenuClick, totalEarnings }: HeaderProps) {
   return (
     <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
       <div className="flex items-center justify-between px-4 py-2">
+        {/* Logo + Name */}
         <div className="flex items-center">
           <Button
             variant="ghost"
@@ -233,6 +237,96 @@ export default function Header({ onMenuClick, totalEarnings }: HeaderProps) {
             </span>
           </Link>
         </div>
+        {/* End Logo + Name */}
+
+        {/* Search + Input Icon > 768px */}
+        {!isMobile && (
+          <div className="flex-1 max-w-xl mx-4">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="search..."
+                className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+              <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+            </div>
+          </div>
+        )}
+        {/* End Search + Input Icon > 768px */}
+
+        {/* Search + Notification */}
+        <div className="flex items-center">
+          {isMobile && (
+            <Button variant="ghost" size="icon" className="mr-2">
+              <Search className="h-5 w-5" />
+            </Button>
+          )}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon" className="mr-2 relative">
+                <Bell className="h-5 w-5 text-gray-800" />
+                {notification.length > 0 && (
+                  <Badge className="absolute -top-1 -right-1 px-1 min-x-[1.2rem] h-5">
+                    {notification.length}
+                  </Badge>
+                )}
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-64">
+              {notification.length > 0 ? (
+                notification.map((notification: any) => (
+                  <DropdownMenuItem
+                    key={notification.id}
+                    onClick={() => handleNotificationClick(notification.id)}>
+                    <div className="flex flex-col">
+                      <span className="font-medium">{notification.type}</span>
+                      <span className="text-sm text-gray-500">
+                        {notification.message}
+                      </span>
+                    </div>
+                  </DropdownMenuItem>
+                ))
+              ) : (
+                <DropdownMenuItem>No new notification</DropdownMenuItem>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <div className="mr-2 md:mr-4 flex items-center bg-gray-100 rounded-full px-2 md:px-3 py-1">
+            <Coins className="h-4 w-4 md:h-5 md:w-5 mr-1 text-green-500" />
+            <span className="font-semibold text-sm md:text-base text-gray-800">
+              {balance.toFixed(2)}
+            </span>
+          </div>
+          {!loggedIn ? (
+            <Button
+              onClick={login}
+              className="bg-green-600 hover:bg-green-700 text-white text-sm md:text-base ">
+              Login
+              <LogIn className="ml-1 md:ml-2 h-4 w-4 md:h5 md:w-5"/>
+            </Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="items-center flex">
+                  <User className="h-5 w-5 mr-1 text-gray-800" />
+                  <ChevronDown className="h-4 w-4 text-gray-800" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={getUserInfo} className="font-bold txt-base text-gray-800">
+                  {userInfo ? userInfo.name : "Profile"}
+                </DropdownMenuItem>
+                <DropdownMenuItem className="font-bold txt-base text-gray-800">
+                  <Link href={"/settings"}>Settings</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={logout} className="font-bold txt-base text-gray-800">
+                  Sign Out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+        {/* End Seach + Notification */}
       </div>
     </header>
   );
